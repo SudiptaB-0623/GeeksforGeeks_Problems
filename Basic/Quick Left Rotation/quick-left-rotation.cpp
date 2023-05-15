e3d6dfaaed7eb12282d7776d7a0747f3102b0b57
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-05-16 00:37:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Quick Left Rotation/quick-left-rotation.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	void leftRotate(int arr[], int k, int n) 
+	{ 
+	   // Your code goes here
+	   vector<int> vcc;
+	   for(int i=k;i<k+n;i++)
+	   {
+	       vcc.push_back(arr[i%n]);
+	   }
+	   for(int i=0;i<n;i++)
+	   {
+	       arr[i] = vcc[i];
+	   }
+	} 
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin >> n;
+	    int k;
+	    cin >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	        // um[a[i]]++;
+	    }
+
+
+       
+
+        Solution ob;
+        ob.leftRotate(a,k,n);
+        
+        for (int i = 0; i < n; i++) 
+        	cout << a[i] << " "; 
+
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
