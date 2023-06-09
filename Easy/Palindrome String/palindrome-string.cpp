a6c7677cf0ddec570e1f9032d86d49b5960d9e32
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-09 23:23:48 +0530

    Updated Solution - GfG to GitHub

--- Easy/Palindrome String/palindrome-string.cpp
+++ Easy/Palindrome String/palindrome-string.cpp
@@ -8,17 +8,25 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-	
+	bool palindrome(int i, string& s)
+	{
+    
+        // Base Condition
+        // If i exceeds half of the string means all the elements 
+        // are compared, we return true.
+        if(i>=s.length()/2) return true;
+        
+        // If the start is not equal to the end, not the palindrome.
+        if(s[i]!=s[s.length()-i-1]) return false;
+        
+        // If both characters are the same, increment i and check start+1 and end-1.
+        return palindrome(i+1,s);
+    }
 	
 	int isPalindrome(string S)
 	{
 	    // Your code goes here
-	    for(int i=0, j=S.size()-1;i<j;i++,j--)
-	    {
-	        if(S[i]!=S[j])
-	            return 0;
-	    }
-	    return 1;
+	    return palindrome(0, S);
 	}
 
 };
