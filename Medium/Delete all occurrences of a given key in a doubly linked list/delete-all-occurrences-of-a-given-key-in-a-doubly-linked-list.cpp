1834e468f0d5a0d468b35940f89dac8dd47881d2
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-16 19:49:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+
+    new_node->prev = NULL;
+
+    new_node->next = (*head_ref);
+
+    if ((*head_ref) != NULL) (*head_ref)->prev = new_node;
+
+    (*head_ref) = new_node;
+}
+
+void printList(struct Node* head) {
+    if (head == NULL) cout << -1;
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+class Solution 
+{
+  public:
+    void deleteAllOccurOfX(struct Node** head, int x) 
+    {
+        // Write your code here
+        if(*head == NULL)
+            return;
+        Node *temp = *head;
+        while(temp!=NULL)
+        {
+            if(temp->data == x)
+            {
+                if(temp == *head)
+                {
+                    *head = (*head)->next;
+                    Node *flag = temp;
+                    temp = temp->next;
+                    if(temp!=NULL)
+                        temp->prev = NULL;
+                    delete flag;
+                }
+                else
+                {
+                    Node *pre = temp->prev;
+                    Node *flag = temp;
+                    temp = temp->next;
+                    pre->next = temp;
+                    if(temp!=NULL)
+                        temp->prev = pre;
+                    delete flag;
+                }
+            }
+            else
+                temp = temp->next;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        Node* head = new Node();
+        head = NULL;
+
+        int n;
+        cin >> n;
+
+        vector<int> inp(n);
+
+        for (int i = 0; i < n; i++) {
+            int val;
+            cin >> val;
+            // push(&head,val);
+            inp[i] = val;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            push(&head, inp[i]);
+        }
+        int key;
+        cin >> key;
+        // printList(head);
+        // cout << endl;
+
+        Solution obj;
+
+        obj.deleteAllOccurOfX(&head, key);
+        printList(head);
+        cout << endl;
+
+        // cout << "\n~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
