5f333825eee0c2540d3f9a2eae81d1f624ba52e2
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-06-24 13:41:49 +0530

    Updated Solution - GfG to GitHub

--- School/Second Largest/second-largest.cpp
+++ School/Second Largest/second-largest.cpp
@@ -12,8 +12,10 @@ public:
 	int print2largest(int arr[], int n) 
 	{
 	    // code here
-	    int max = arr[0], max2 = -1;
-	    for(int i=1;i<n;i++)
+	    int max = -1;
+	    int max2 = -1;
+	    
+	    for(int i=0;i<n;i++)
 	    {
 	        if(arr[i]>max)
 	        {
@@ -21,9 +23,7 @@ public:
 	            max = arr[i];
 	        }
 	        else if(arr[i]>max2 && arr[i]!=max)
-	        {
 	            max2 = arr[i];
-	        }
 	    }
 	    return max2;
 	}
