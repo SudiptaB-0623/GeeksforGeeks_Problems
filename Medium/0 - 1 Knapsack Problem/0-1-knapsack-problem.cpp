f872ab4343518fc6b93a8f189bd0b549a46384f8
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-09 01:14:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -8,31 +8,49 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int recur(int w, int wt[], int val[], int n, vector<vector<int>> &dp)
-    {
-        if(dp[n][w] != -1)
-            return dp[n][w];
+    // int recur(int w, int wt[], int val[], int n, vector<vector<int>> &dp)
+    // {
+    //     if(dp[n][w] != -1)
+    //         return dp[n][w];
             
-        if(n == 0)
-        {
-            if(wt[n] <= w)
-                return val[n];
-            else
-                return 0;
-        }
+    //     if(n == 0)
+    //     {
+    //         if(wt[0] <= w)
+    //             return val[n];
+    //         else
+    //             return 0;
+    //     }
         
-        int take = 0;
-        if(wt[n] <= w)
-            take = val[n] + recur(w-wt[n], wt, val, n-1, dp);
-        int notTake = recur(w, wt, val, n-1, dp);
+    //     int take = 0;
+    //     if(wt[n] <= w)
+    //         take = val[n] + recur(w-wt[n], wt, val, n-1, dp);
+    //     int notTake = recur(w, wt, val, n-1, dp);
         
-        return dp[n][w] = max(take, notTake);
-    }
+    //     return dp[n][w] = max(take, notTake);
+    // }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n, vector<int>(W+1, -1));
-       return recur(W, wt, val, n-1, dp);
+       vector<vector<int>> dp(n, vector<int>(W+1, 0));
+        for(int i=wt[0]; i<=W ; i++)
+        {
+            dp[0][i] = val[0];
+        }
+        
+        for(int i = 1; i<n ; i++)
+        {
+            for(int j = 0; j<=W ; j++)
+            {
+                int take = INT_MIN;
+                if(wt[i] <= j)
+                    take = val[i] + dp[i-1][j-wt[i]];
+                int notTake = dp[i-1][j];
+                
+                dp[i][j] = max(take, notTake);
+            }
+        }
+        
+        return dp[n-1][W];
     }
 };
 
