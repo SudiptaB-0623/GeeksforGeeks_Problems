d7d7af47827647d3ae501f9bd50fd35d0c992149
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-07-12 00:55:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Help Classmates/help-classmates.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes here
+        vector<int> ans(n);
+        stack<int> st;
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            if(i==n-1)
+            {
+                ans[i] = -1;
+                st.push(arr[i]);
+            }
+            else
+            {
+                while(st.empty()!=true && st.top()>=arr[i])
+                    st.pop();
+                
+                if(st.empty())
+                    ans[i] = -1;
+                else
+                    ans[i] = st.top();
+                
+                st.push(arr[i]);
+            }
+        }
+        
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+
+// } Driver Code Ends
