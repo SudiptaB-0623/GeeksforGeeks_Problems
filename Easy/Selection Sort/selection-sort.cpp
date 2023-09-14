b573d5b937155bd4840e83afafdedf98d380d6f3
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-09-14 11:41:42 +0530

    Updated Solution - GfG to GitHub

--- Easy/Selection Sort/selection-sort.cpp
+++ Easy/Selection Sort/selection-sort.cpp
@@ -13,25 +13,23 @@ void swap(int *xp, int *yp)
 class Solution
 {
     public:
-    int select(int arr[], int i, int n)
+    int select(int arr[], int i)
     {
         // code here such that selectionSort() sorts arr[]
-        int mini = i;
-        for(int j=i+1; j<n ; j++)
-        {
-            if(arr[j] < arr[mini])
-                mini = j;
-        }
         
-        return mini;
     }
      
     void selectionSort(int arr[], int n)
     {
        //code here
-       for(int i=0 ; i<n ; i++)
+       for(int i=0 ; i<n-1 ; i++)
        {
-           int mini = select(arr, i, n);
+           int mini = i;
+           for(int j=i+1 ; j<n ; j++)
+           {
+                if(arr[j] < arr[mini])
+                    mini = j;
+           }
            swap(arr[i], arr[mini]);
        }
     }
