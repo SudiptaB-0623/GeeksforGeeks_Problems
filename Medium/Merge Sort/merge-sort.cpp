e9354184acd2ca4156d3df26c04d6a7be4083865
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-09-14 19:49:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort/merge-sort.cpp
+++ Medium/Merge Sort/merge-sort.cpp
@@ -21,69 +21,62 @@ class Solution
     public:
     void merge(int arr[], int l, int m, int r)
     {
-         // Your code here
-         //Creating two arrays m1, m2 of size a, b respectively to store the elements of each half in it
-
-        int a = m-l+1;
-        int b = r-m;
-    
-        int m1[a], m2[b];
-    
-        for(int i=0;i<a;i++)
+        // Your code here
+        int s1 = m-l+1;
+        int s2 = r-m;
+        
+        
+        int L[s1];
+        int R[s2];
+        
+        for(int i=0 ; i<s1 ; i++)
         {
-            m1[i]=arr[l+i];
+            L[i] = arr[l+i];
         }
-        for(int i=0;i<b;i++)
+        for(int i=0 ; i<s2 ; i++)
         {
-            m2[i]=arr[m+1+i];
+            R[i] = arr[m+i+1];
         }
-    
-        //Comparing the arrays m1, m2 and merging them in sorted order
-    
-        for(int i=0,j=0,k=0;(i<a)||(j<b);)
+        
+        int i=0, j=0, k=l;
+        while(i<s1 && j<s2)
         {
-            if(i>=a)                            //if m1 runs out of element, just keep adding elements from m2
-            {            
-                arr[l+k]=m2[j];
-                j++;
-                k++;
-                continue;
-            }
-            if(j>=b)                            //if m2 runs out of element, just keep adding elements from m1
+            if(L[i] < R[j])
             {
-                arr[l+k]=m1[i];
-                i++;
-                k++;
-                continue;
-            }
-    
-            //Compare and merge elements accordingly
-    
-            if(m1[i]<m2[j])
-            {
-                arr[l+k]=m1[i];
+                arr[k] = L[i];
                 i++;
             }
             else
             {
-                arr[l+k]=m2[j];
+                arr[k] = R[j];
                 j++;
             }
             k++;
         }
+        while(i<s1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j<s2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
     public:
-    void mergeSort(int arr[], int left, int right)
+    void mergeSort(int arr[], int l, int r)
     {
         //code here
-        if(left<right)                              
-        {                                           
-            int mid = (left+right)/2;
-    
-            mergeSort(arr,left,mid);                //Left half of the broken array part
-            mergeSort(arr,mid+1,right);             //Right half of the broken array part
-            merge(arr,left,mid,right);              //Merging the broken halfs in sorted order
-        }
+        if(l>=r)
+            return;
+        
+        int mid = (l+r)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
     }
 };
 
