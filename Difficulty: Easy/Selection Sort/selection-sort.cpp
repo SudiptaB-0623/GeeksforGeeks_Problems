ea38b873c9aa555f9b07037ad6c4d25dd22e7667
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2024-08-24 22:12:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.cpp
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -13,27 +13,23 @@ void swap(int *xp, int *yp)
 class Solution
 {
     public:
-    int select(int arr[], int i, int n)
-    {
-        // code here such that selectionSort() sorts arr[]
-        int mini = i;
-        for(int j=i+1; j<n ; j++) {
-            if(arr[j]<arr[mini]) {
-                mini = j;
-            }
-        }
-        swap(arr[i], arr[mini]);
-    }
-     
     void selectionSort(int arr[], int n)
     {
        //code here
-       for(int i=0; i<=n-2; i++) {
-           select(arr, i, n);
+       for(int i=0 ; i<n ; i++)
+       {
+            int min_ind = i;
+            for(int j=i+1; j<n ; j++)
+            {
+                if(arr[j]<arr[min_ind])
+                {
+                    min_ind = j;
+                }
+            }
+            swap(arr[i], arr[min_ind]);
        }
     }
 };
-
 
 //{ Driver Code Starts.
  
